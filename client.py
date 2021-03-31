757dd63e890dc9e96777dc82595518b02f31a58f
t <469415@mail.muni.cz>
2021-03-31 12:24:22 +0200

    Rewrite client and server as classes

--- client.py
+++ client.py
@@ -6,36 +6,54 @@ from noise.connection import NoiseConnection, Keypair
 
 import constants
 
-def main():
-    sock = socket.socket()
-    sock.connect(("localhost", constants.SERVER_PORT))
+
+class Client:
+    def __init__(self):
+        self.sock = socket.socket()
+        self.key_pair = ED25519().generate_keypair()
+
+    def exchange_public_keys(self) -> bytes:
+        self.sock.send(self.key_pair.public_bytes)
+        return self.sock.recv(constants.SERVER_PORT)
+
+    def set_connection_keys(self):
+        server_public_key = self.exchange_public_keys()
+        self.noise = NoiseConnection.from_name(b"Noise_KK_25519_AESGCM_SHA256")
+        self.noise.set_keypair_from_private_bytes(
+            Keypair.STATIC,
+            self.key_pair.private.private_bytes(
+                format=serialization.PrivateFormat.Raw,
+                encoding=serialization.Encoding.Raw,
+                encryption_algorithm=serialization.NoEncryption(),
+            ),
+        )
+        self.noise.set_keypair_from_public_bytes(Keypair.REMOTE_STATIC, server_public_key)
+
+    def noise_handshake(self):
+        self.noise.set_as_initiator()
+        self.noise.start_handshake()
+        message = self.noise.write_message()
+        self.sock.send(message)
+        received = self.sock.recv(constants.CLIENT_PORT)
+        payload = self.noise.read_message(received)
+
+    def send_encrypted_msg(self, message: str):
+        encrypted_message = self.noise.encrypt(bytes(message, encoding="UTF-8"))
+        self.sock.send(encrypted_message)
+
+    def receive_and_decrypt_msg(self):
+        ciphertext = self.sock.recv(constants.CLIENT_PORT)
+        plaintext = self.noise.decrypt(ciphertext)
+        print(plaintext)
+
+    def run(self):
+        self.sock.connect(("localhost", constants.SERVER_PORT))
+        self.set_connection_keys()
+        self.noise_handshake()
+        self.send_encrypted_msg("Hello server!")
+        self.receive_and_decrypt_msg()
 
 
-    key_pair = ED25519().generate_keypair()
-    sock.send(key_pair.public_bytes)
-    server_public_key = sock.recv(constants.SERVER_PORT)
-
-    noise = NoiseConnection.from_name(b"Noise_KK_25519_AESGCM_SHA256")
-    noise.set_keypair_from_private_bytes(Keypair.STATIC, key_pair.private.private_bytes(
-        format=serialization.PrivateFormat.Raw,
-        encoding=serialization.Encoding.Raw,
-        encryption_algorithm=serialization.NoEncryption()
-    ))
-    noise.set_keypair_from_public_bytes(Keypair.REMOTE_STATIC, server_public_key)
-
-    noise.set_as_initiator()
-    noise.start_handshake()
-    message = noise.write_message()
-    sock.send(message)
-    received = sock.recv(constants.CLIENT_PORT)
-    payload = noise.read_message(received)
-
-    encrypted_message = noise.encrypt(b"Hello server!")
-    sock.send(encrypted_message)
-
-    ciphertext = sock.recv(constants.CLIENT_PORT)
-    plaintext = noise.decrypt(ciphertext)
-    print(plaintext)
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    client = Client()
+    client.run()
