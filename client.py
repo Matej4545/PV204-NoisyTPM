4f3003b90a083ccae8f861fbbc129491f37954c2
Matěj Groman <matej.groman98@gmail.com>
2021-04-20 22:38:35 +0200

    Add argument parser, multiple msg, interactive

--- client.py
+++ client.py
@@ -5,7 +5,8 @@ from noise.backends.default.diffie_hellmans import ED25519
 from noise.connection import NoiseConnection, Keypair
 from sys import argv
 import constants
-
+import argparse
+import threading
 
 class Client:
     def __init__(self):
@@ -34,29 +35,72 @@ class Client:
         self.noise.start_handshake()
         message = self.noise.write_message()
         self.sock.send(message)
-        received = self.sock.recv(constants.CLIENT_PORT)
+        received = self.sock.recv(4096)
         payload = self.noise.read_message(received)
 
     def send_encrypted_msg(self, message: str):
         encrypted_message = self.noise.encrypt(bytes(message, encoding="UTF-8"))
         self.sock.send(encrypted_message)
 
+    def send_messages(self):
+        print("You can write messages now, [q, quit, exit] to quit:")
+        while True:
+            user_input = input("Message: ")
+            if len(user_input) == 0:
+                print("Please input valid message, [q, quit, exit] to quit.")
+                continue
+            if user_input.lower() in ["q", "quit", "exit"]:
+                return
+            self.sock = socket.socket()
+            self.sock.connect((self.ip, self.port))
+            self.set_connection_keys()
+            self.noise_handshake()
+            self.send_encrypted_msg(user_input)    
+            self.receive_and_decrypt_msg()
+            self.sock.close()
+
     def receive_and_decrypt_msg(self):
-        ciphertext = self.sock.recv(constants.CLIENT_PORT)
+        ciphertext = self.sock.recv(4096)
         plaintext = self.noise.decrypt(ciphertext)
-        print(plaintext)
+        print(f'Server: {plaintext.decode("utf-8") }')
 
-    def run(self, message):
-        self.sock.connect(("localhost", constants.SERVER_PORT))
-        self.set_connection_keys()
-        self.noise_handshake()
-        self.send_encrypted_msg(message)
-        self.receive_and_decrypt_msg()
+    def register(self):
+        #TODO: read TPMs PCR values and somehow send them to a server
+        print("Registration complete, your identifier is <TOP_SECRET_PCR_HASH>")
+        return True
 
+    def run(self, ip, port, message):
+        self.ip = ip
+        self.port = port
+        if (message): # One time
+            self.sock.connect((ip, port))
+            self.set_connection_keys()
+            self.noise_handshake()
+            self.send_encrypted_msg(message)
+            self.receive_and_decrypt_msg()
+        else: # Multiple messages
+            self.send_messages()
+        
 
 if __name__ == "__main__":
-    client = Client()
-    if len(argv) > 1:
-        client.run(argv[1])
-    else:
-        print("Please write message as argument")
+    parser = argparse.ArgumentParser(description='PV204 NoisyTPM - this is a part of team project for PV204. \
+                                                Client app can communicate with server using Noise framework \
+                                                and authenticate via TPM. Please see \
+                                                \'https://github.com/Matej4545/PV204-NoisyTPM/\' for more info.')
+    parser.add_argument('-s','--server', dest='server', metavar='IP', type=str, nargs=1, default='localhost', help="An IP address or hostname of the server.")
+    parser.add_argument('-p','--port', dest='port', metavar='PORT', type=int, nargs=1, default=5555, help="A port where the server is listening.")
+    parser.add_argument('-m', '--message', metavar='MESSAGE', dest='message', type=str, nargs='+', help='Specify message as argument. For interactive session please omit.') 
+    parser.add_argument('-r --register', dest="register", action='store_true', default=False, help="If you are not authenticated or running the app first time, you will need to register.")
+    args=parser.parse_args()
+ 
+    try:
+        message =  "" if args.message is None else "".join(args.message).strip()
+        server = args.server[0].strip()
+        port = args.port[0]
+        client = Client()
+        if args.register:
+            client.register()
+        client.run(server, port, message)
+    except Exception as e:
+        print("An error occured! Quitting app.")
+        print(e)
